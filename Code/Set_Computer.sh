dff803ac9e9b03e9e4862dbfb3bced1a78b40b3d
Matthew Elliott <melliot1@ucsc.edu>
2021-02-21 15:08:28 -0800

    Update Set_Computer.sh

--- Code/Set_Computer.sh
+++ Code/Set_Computer.sh
@@ -1,11 +1,12 @@
 
-VERSION_NAME=v3.0.1
-# Set io's Version name in custom.js file
-sed -i "s/{VERSION_NAME}/$VERSION_NAME/g"  ~/WetAI_Terra_Docker/Code/Set_Interface.js
 
 ######################
 # Wet AI Interface
 ######################
+VERSION_NAME=v3.0.1
+# Set io's Version name in custom.js file
+sed -i "s/{VERSION_NAME}/$VERSION_NAME/g"  ~/WetAI_Terra_Docker/Code/Set_Interface.js
+
 #mkdir ~/.jupyter/custom && mv ~/WetAI_Docker/Code/Set_Interface.js ~/.jupyter/custom/custom.js 
 sed -i '$d' ~/.jupyter/custom/custom.js
 echo "  require(['custom/Set_Interface']);" >> ~/.jupyter/custom/custom.js
