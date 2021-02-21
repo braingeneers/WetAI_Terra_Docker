112d266474ef323afc7c5aab63bd78472c4854af
Matthew Elliott <melliot1@ucsc.edu>
2021-02-21 15:08:08 -0800

    Update Set_Computer.sh

--- Code/Set_Computer.sh
+++ Code/Set_Computer.sh
@@ -3,18 +3,19 @@ VERSION_NAME=v3.0.1
 # Set io's Version name in custom.js file
 sed -i "s/{VERSION_NAME}/$VERSION_NAME/g"  ~/WetAI_Terra_Docker/Code/Set_Interface.js
 
+######################
+# Wet AI Interface
+######################
 #mkdir ~/.jupyter/custom && mv ~/WetAI_Docker/Code/Set_Interface.js ~/.jupyter/custom/custom.js 
 sed -i '$d' ~/.jupyter/custom/custom.js
 echo "  require(['custom/Set_Interface']);" >> ~/.jupyter/custom/custom.js
-
+echo "});" >> ~/.jupyter/custom/custom.js
 #pip install plotly
 
 
 
 
-######################
-# Wet AI Interface
-######################
+
 
 
 
