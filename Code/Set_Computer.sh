414241f36faad68dc729f2229f3e9bfbdb864ac9
Matthew Elliott <melliot1@ucsc.edu>
2021-02-21 15:03:08 -0800

    Update Set_Computer.sh

--- Code/Set_Computer.sh
+++ Code/Set_Computer.sh
@@ -4,9 +4,9 @@ VERSION_NAME=v3.0.1
 sed -i "s/{VERSION_NAME}/$VERSION_NAME/g"  ~/WetAI_Terra_Docker/Code/Set_Interface.js
 
 #mkdir ~/.jupyter/custom && mv ~/WetAI_Docker/Code/Set_Interface.js ~/.jupyter/custom/custom.js 
-sed '$d' ~/.jupyter/custom/custom.js 
+sed -i '$d' ~/.jupyter/custom/custom.js
 
-pip install plotly
+#pip install plotly
 
 
 
