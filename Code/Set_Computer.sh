45233248eda5584f53ff9f7f50e64d853dd2ee41
Matthew Elliott <melliot1@ucsc.edu>
2021-02-20 21:59:15 -0800

    Create Set_Computer.sh

---
+++ Code/Set_Computer.sh
@@ -0,0 +1,15 @@
+
+VERSION_NAME=v3.0.0
+# Set io's Version name in custom.js file
+#sed -i "s/{VERSION_NAME}/$VERSION_NAME/g"  ~/WetAI_Docker/Code/Set_Interface.js
+
+
+pip install plotly
+
+
+
+
+
+
+
+
