3d9c8c7c06b26683212fb1eefb64160348685488
Matthew Elliott <melliot1@ucsc.edu>
2021-03-03 21:34:32 -0800

    Update Set_Computer.sh

--- Code/Set_Computer.sh
+++ Code/Set_Computer.sh
@@ -22,8 +22,7 @@ conda install -y -c conda-forge  rise.  # not visible dunno why
 conda install -y -c conda-forge jupyter_contrib_nbextensions
 conda install -y jupyterlab
 jupyter nbextensions_configurator enable
-for package in collapsible_headings/main notify/notify toc2/main codefolding/main hide_header/main hide_input_all/main table_beautifier/main codefolding/edit contrib_nbextensions_help_item/main python-markdown/main move_selected_cells/main splitcell/splitcell tree-filter/index; do jupyter nbextension enable $package; done;
-jupyter nbextension enable help_panel/help_panel # not visible, dunno why
+for package in collapsible_headings/main notify/notify toc2/main codefolding/main hide_header/main hide_input_all/main table_beautifier/main codefolding/edit contrib_nbextensions_help_item/main python-markdown/main move_selected_cells/main help_panel/help_panel splitcell/splitcell tree-filter/index; do jupyter nbextension enable $package; done;
 #jupyter nbextension enable varInspector/main # Cool but removes \tab autocomplete
 
 # Set Files from wet_io Github
