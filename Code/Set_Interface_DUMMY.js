fb4a2498f700d4249f1be330f8b1fda121e7ee08
Matthew Elliott <melliot1@ucsc.edu>
2022-04-19 16:02:20 -0700

    Create Set_Interface_DUMMY.js

---
+++ Code/Set_Interface_DUMMY.js
@@ -0,0 +1,6 @@
+// Change Main Page View
+if( document.getElementById("tab_content") != null ){
+    var logo = document.getElementById("ipython_notebook");
+    logo.style.paddingTop = "6px";
+    logo.innerHTML += '<font size="6" face="Palatino" color="blue">&nbsp; &nbsp; <i>wet<b>AI</b></i></font>';
+}
